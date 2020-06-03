8c680b3d4bd7f2c708a3a29e13d05fd6d8af85a5
Kidane <kyw@iduce.org>
2020-06-03 19:12:46 +0100

    Co-authored-by: sunmingac <sunmingac@gmail.com>

--- build.sbt
+++ build.sbt
@@ -5,3 +5,6 @@ libraryDependencies ++= Seq(
   "io.circe" %% "circe-generic",
   "io.circe" %% "circe-parser"
 ).map(_ % circeVersion)
+
+libraryDependencies += "org.typelevel" %% "cats-core" % "2.1.0"
+libraryDependencies += "org.typelevel" %% "cats-effect" % "2.1.3"
--- src/main/scala/repository/CourseRepos.scala
+++ src/main/scala/repository/CourseRepos.scala
@@ -1,12 +1,39 @@
 package repository
 import java.util.UUID
 import model._
+import cats.effect._
+import cats.effect.implicits._
+import java.{util => ju}
+import scala.collection.mutable.Map
 
 trait CourseRepo[F[_]] {
-    def getCourse(id: UUID): F[Course]
+    def getCourse(id: UUID): F[Option[Course]]
     def createCourse(name: String): F[Course]
     def deleteCouse(id: UUID): F[Unit]
-    def modifyCourse(id: UUID, course: Course): F[Course]
+    def modifyCourse(id: UUID, course: Course): F[Option[Course]]
 }
 
-case class CourseRepoInMem()
+case class CourseRepoInMem[F[_]](env: Map[UUID, Course])(implicit F: Sync[F]) extends CourseRepo[F]{
+
+    override def getCourse(id: ju.UUID): F[Option[Course]] = F.delay {
+        env.get(id)
+    }
+
+    override def deleteCouse(id: ju.UUID): F[Unit] = F.delay {
+        env.-(id)
+    }
+
+    override def modifyCourse(id: ju.UUID, course: Course): F[Option[Course]] =  {
+        env -= id
+        env += Map(id, course)
+        getCourse(id)
+    }
+
+    override def createCourse(name: String): F[Course] = F.delay {
+        val id = UUID.randomUUID()
+        val course = Course(id, name)
+        env += Map(id -> course)
+        course
+    }
+
+}
